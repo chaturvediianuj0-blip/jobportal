fe20851e53945c34773a39a75932188e1b7644d6
Dev Two <dev2@jobportal.com>
2026-09-07 06:25:38 +0000

    feat(jobs): add job sorting

--- jobs.py
+++ jobs.py
@@ -4,3 +4,5 @@ def list_jobs(): return []
 def search_jobs(): pass
 def search_jobs(keyword):
     return [job for job in list_jobs() if keyword in job]
+def sort_jobs(jobs, key='title'):
+    return sorted(jobs, key=lambda j: j.get(key, ''))
